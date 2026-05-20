5e798fea9617756478540d6fec567c74d9c6fd0a
Sayem Islam <sayem.shuvo999@gmail.com>
2026-05-20 11:58:58 +0600

    class-4 problem-3

---
+++ class_4_practice.py
@@ -0,0 +1,20 @@
+#problem -3
+#Story: You found an ancient scroll (a string). Legend says to unlock the chamber, you must find every unique character, calculate the square of its ASCII value, and store them in a set.
+
+import sys
+#1.Accept a string via Command Line Arguments (sys.argv). If none provided, default to "magic".
+text=sys.argv[1] if len(sys.argv)>1 else "magic"
+print(text)
+
+#2.Use a Set Comprehension and a Lambda to generate a set of squared ASCII values (ord(char)**2) for every unique character in the string.
+
+f=lambda x:ord(x)**2
+
+s={f(x) for x in set(text) }
+print(s)
+# 3.Write the resulting set to a text file runes.txt.
+with open("runs.txt",'w') as f:
+    f.write(str(s))
+
+with open("runs.txt",'r') as f:
+    print(f.read())
